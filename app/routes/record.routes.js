771b670e0d22c29dbf5207c89a7e2168eee05ab8
rohitcube <104619746+rohitcube@users.noreply.github.com>
2023-05-27 18:59:28 +0800

    Add files via upload

---
+++ app/routes/record.routes.js
@@ -0,0 +1,29 @@
+module.exports = app => {
+    const records = require("../controllers/record.controller.js");
+  
+    var router = require("express").Router();
+  
+    // Create a new record
+    router.post("/", records.create);
+  
+    // Retrieve all records
+//    router.get("/", records.findAll);
+  
+    // Retrieve all published records
+//    router.get("/published", records.findAllPublished);
+  
+    // Retrieve a single record with id
+    router.get("/:stu_num", records.findOne);
+  
+    // Update a record with id
+    router.put("/:id", records.update);
+  
+    // Delete a record with id
+    router.delete("/:id", records.delete);
+
+  
+    // Delete all records
+  //  router.delete("/", records.deleteAll);
+  
+    app.use('/api/records', router);
+  };
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+
+console.log("bruh");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:3031"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./app/models");
+db.sequelize.sync( { alter: true } )
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Gymbro!" });
+});
+
+require("./app/routes/record.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 3031;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
